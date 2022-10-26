a31d219db84a00f9105220a8e47aec9b42de7ced
Kshitijv24 <kshitijv24@gmail.com>
2022-10-26 12:14:42 +0530

    Created cube prefab with a text labeler on top of it which shows the coordinates of the cube using CubeEditor.cs Script
    and added some code into our code editor script which adds restrictions by which our game objects can only move by the increment of 10 unites and not freely.

---
+++ Tower-Defense-2/Assets/Scripts/CubeEditor.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using TMPro;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 20f)] float gridSize;
+
+    TextMeshPro textMeshPro;
+
+    private void Update()
+    {
+        Vector3 snapPos;
+
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        textMeshPro = GetComponentInChildren<TextMeshPro>();
+        textMeshPro.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+    }
+}
