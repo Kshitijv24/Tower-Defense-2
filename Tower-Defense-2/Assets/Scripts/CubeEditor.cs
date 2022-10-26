80db75efd72920709d7939992ee2ba18cde8b103
Kshitijv24 <kshitijv24@gmail.com>
2022-10-26 13:04:06 +0530

    moving our enemy on the path made of cubes using enemy movement script

--- Tower-Defense-2/Assets/Scripts/CubeEditor.cs
+++ Tower-Defense-2/Assets/Scripts/CubeEditor.cs
@@ -18,6 +18,9 @@ public class CubeEditor : MonoBehaviour
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
 
         textMeshPro = GetComponentInChildren<TextMeshPro>();
-        textMeshPro.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+        
+        string labelText = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+        textMeshPro.text = labelText;
+        gameObject.name = labelText;
     }
 }
---
+++ Tower-Defense-2/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path;
+
+    private void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        foreach (Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+}
