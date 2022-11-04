6991ef0fbadc8a86f2af470e398eaec949408832
Kshitijv24 <kshitijv24@gmail.com>
2022-11-04 13:13:13 +0530

    designed a level for the game and created a particle effect for enemy when it reaches it's destination

--- Tower-Defense-2/Assets/Scripts/EnemyMovement.cs
+++ Tower-Defense-2/Assets/Scripts/EnemyMovement.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 
 public class EnemyMovement : MonoBehaviour
 {
+    [SerializeField] float movementPeriod;
+    [SerializeField] ParticleSystem goalParticle;
+
     private void Start()
     {
         Pathfinder pathfinder = FindObjectOfType<Pathfinder>();
@@ -16,7 +19,17 @@ public class EnemyMovement : MonoBehaviour
         foreach (Waypoint waypoint in path)
         {
             transform.position = new Vector3(waypoint.transform.position.x, waypoint.transform.position.y + 10, waypoint.transform.position.z);
-            yield return new WaitForSeconds(2f);
+            yield return new WaitForSeconds(movementPeriod);
         }
+
+        SelfDestruct();
+    }
+
+    private void SelfDestruct()
+    {
+        ParticleSystem vfx = Instantiate(goalParticle, transform.position, Quaternion.identity);
+        vfx.Play();
+        Destroy(vfx.gameObject, vfx.main.duration);
+        Destroy(gameObject);
     }
 }
